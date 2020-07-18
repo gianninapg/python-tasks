9d42f1b1c1468eb43eedc8740910041f98db2efb
gianninapg@gmail.com <Gi@090488>
2020-07-18 21:53:12 +1000

    eq

--- equation.py
+++ equation.py
@@ -8,14 +8,15 @@ print(type(first_int))
 result = first_int * second_int
 print(result)
 
+
 first_int = -3
 second_int = 9
 print(type(first_int))
-result = first_int * second_int
+print(first_int * second_int)
 
-first_int = -3.0
-second_int = 9
-
-print(type(first_int))
-
-result = first_int * second_int
+print("Floats")
+#This section is about floats
+first_float = 3.0
+second_float = -9
+print(type(second_float))
+print(first_float * second_float)
