f1220214bdae9adcf040ecca071dd76c70774b8e
gianninapg@gmail.com <Gi@090488>
2020-07-18 21:42:41 +1000

    calculate

--- calculate.py
+++ calculate.py
@@ -1,11 +1,28 @@
 print("Integers")
 
-first_int = 1
-second_int = 3
+first_int = 3
+second_int = 9
 
 print(type(first_int))
-print(second_int)
+#print(second_int)
 
 result = first_int + second_int
+
 print(result)
+
+
+first_int = 9
+second_int = 3
+
 print(first_int - second_int)
+
+
+print("Floats")
+#This section is about floats
+
+first_float = 3.0
+second_float = -9
+
+print(type(second_float))
+
+print(first_float + second_float)
---
+++ equation.py
@@ -0,0 +1,11 @@
+print("Integers")
+
+first_int = 3
+second_int = 9
+
+print(type(first_int))
+#print(second_int)
+
+result = first_int * second_int
+
+print(result)
