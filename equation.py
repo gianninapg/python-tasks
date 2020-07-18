4e0807b4a61fa1f299462e2751605a100efd4609
gianninapg@gmail.com <Gi@090488>
2020-07-18 21:48:55 +1000

    equation.

--- equation.py
+++ equation.py
@@ -2,10 +2,20 @@ print("Integers")
 
 first_int = 3
 second_int = 9
-
 print(type(first_int))
 #print(second_int)
 
 result = first_int * second_int
-
 print(result)
+
+first_int = -3
+second_int = 9
+print(type(first_int))
+result = first_int * second_int
+
+first_int = -3.0
+second_int = 9
+
+print(type(first_int))
+
+result = first_int * second_int
