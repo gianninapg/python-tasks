f51588acabbe4f50740a2393677d99a6e0ab4d8c
gianninapg@gmail.com <Gi@090488>
2020-07-21 22:14:05 +1000

    conditional exercises completed.

--- height.py
+++ height.py
@@ -1,12 +1,12 @@
 your_height = int(input("Your height: "))
 print()
-if your_height < 0:
-    print("This cannot be true!")
-elif your_height == 120:
+# if your_height < 0:
+#     print("This cannot be true!")
+if your_height >= 120:
     print("Hop on!")
-elif your_height == 190:
+elif your_height == 191:
     print("Hop on!")
 else:
     your_height = 50
-    print("Sorry, you cannot hop on!")
+    print("Sorry, not today :(")
 
