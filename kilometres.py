180b30eb86084f828eab0cae939564cef7a9aa71
gianninapg@gmail.com <Gi@090488>
2020-07-21 16:37:27 +1000

    km to metres and centimetres

---
+++ kilometres.py
@@ -0,0 +1,15 @@
+# take input from user
+kilometers = float(input("Enter value in kilometers: "))
+ 
+# calculate metres
+meter = kilometers * 1000
+ 
+print('%0.2fkm = %0.2f m' %(kilometers,meter))
+
+ 
+# calculate metres to cm
+centimeter = kilometers * 100000
+ 
+print('%0.2fkm = %0.2f cm' %(kilometers,meter))
+
+
