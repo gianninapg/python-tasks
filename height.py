1b2c07484cd0c671a7bf87fe2e6f073c6c9ce849
gianninapg@gmail.com <Gi@090488>
2020-07-21 22:05:06 +1000

    resolved height exercise

---
+++ height.py
@@ -0,0 +1,12 @@
+your_height = int(input("Your height: "))
+print()
+if your_height < 0:
+    print("This cannot be true!")
+elif your_height == 120:
+    print("Hop on!")
+elif your_height == 190:
+    print("Hop on!")
+else:
+    your_height = 50
+    print("Sorry, you cannot hop on!")
+
