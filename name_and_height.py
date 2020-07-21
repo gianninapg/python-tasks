9e47aae3c2d224d3805621096c3bb8eb8e9d334a
gianninapg@gmail.com <Gi@090488>
2020-07-21 16:45:32 +1000

    name and height task

---
+++ name_and_height.py
@@ -0,0 +1,15 @@
+name = "William"
+height = "192"
+
+print(name)
+print(height)
+
+print(f"{name} is {height}cms tall")
+
+name = "Roary"
+height = "27"
+
+print(name)
+print(height)
+
+print(f"{name} is {height}cms tall")
