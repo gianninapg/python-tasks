fea3aabcb47bfc327437a5968d56522c26e2a3ca
gianninapg@gmail.com <Gi@090488>
2020-07-21 17:25:12 +1000

    created if hunting conditional

---
+++ if_statement_hunting.py
@@ -0,0 +1,15 @@
+moths_in_house = True
+mitch_is_home = True
+
+if moths_in_house:
+    print("Hoooman! Help me get the moths!.")
+if mitch_is_home:
+    print("Hoooman! Help me get the moths!")
+
+#moths_in_house = False
+#mitch_is_home = False
+
+ #   if moths_in_house:
+  #      print("No threads detected")
+   # if mitch_is_home:
+    #    print("No threads detected")
