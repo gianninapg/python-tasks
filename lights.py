ebcabd6dab3665be19b44dd7979542dd85d1009a
gianninapg@gmail.com <Gi@090488>
2020-07-21 20:49:05 +1000

    updated lights

--- lights.py
+++ lights.py
@@ -1,3 +1,25 @@
 light_colour = "Red"
 car_detected = False
+print("Do nothing")
+
+light_colour = "Red"
+car_detected = True
+print("Flash!")
+
+
+light_colour = "Green"
+car_detected = False
+print("Do nothing")
+
+light_colour = "Green"
+car_detected = True
+print("Do nothing")
+
+light_colour = "Amber"
+car_detected = False
+print("Do nothing")
+
+light_colour = "Amber"
+car_detected = True
+print("Do nothing")
 
