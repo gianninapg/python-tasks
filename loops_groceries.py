5137e1dce918c9af9105ae71391f3a9e45b54bb2
gianninapg@gmail.com <Gi@090488>
2020-07-25 21:18:04 +1000

    update loops

--- loops_groceries.py
+++ loops_groceries.py
@@ -10,7 +10,7 @@ groceries = [
 quantity = int(input("How many of each item did you buy?"))
 
 for item in groceries:
-
+    print("Izzy's Food Emporium")
     print(f"{item[0]:<20} ${item[1] * quantity:.2f}")
  #   print(sum(item[1])
 
@@ -18,6 +18,13 @@ for item in groceries:
 #     print(item, price)
 
 # sum1 = 0
-# for item in groceries:
-#     sum1 += int(item)
-# print("Sum of list = ", sum1)
+# for num in groceries:
+#     sum1 += int(num)
+#    print("Sum of list = ", sum1)
+
+
+
+#Option 2 for printing items
+#items can be any name
+# for item, price in groceries:
+#     print(item, price)
