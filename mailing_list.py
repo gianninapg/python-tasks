6dc97e9395a88d570aa92db2590714c8ece40633
gianninapg@gmail.com <Gi@090488>
2020-07-25 11:15:27 +1000

    Created mailing_list.py

---
+++ mailing_list.py
@@ -0,0 +1,19 @@
+mailing_list = [
+["Roary", "roary@moth.catchers"],
+["Remus", "remus@kapers.dog"],
+["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+["Biscuit", "biscuit@whippies.park"],
+["Rory", "rory@whippies.park"],
+]
+
+mailing_list [0] = "Roary : roary@moth.catchers"
+mailing_list [1] = "Remus : remus@kapers.dog"
+mailing_list [2] = "Remus : remus@kapers.dog"
+mailing_list [3] = "Prince Thomas of Whitepaw : hrh.thomas@royalty.wp"
+mailing_list [4] = "Rory : rory@whippies.park"
+
+print(mailing_list [0])
+print(mailing_list [1])
+print(mailing_list [2])
+print(mailing_list [3])
+print(mailing_list [4])
