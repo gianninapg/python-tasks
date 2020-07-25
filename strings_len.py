036e1ad2e0d98e4a0f993492f7422bd1582bd032
gianninapg@gmail.com <Gi@090488>
2020-07-25 15:34:16 +1000

    strings len

--- strings_len.py
+++ strings_len.py
@@ -1,3 +1,6 @@
 input_string = input("Enter a word")
 
-print(len (input_string), [f"{input_string}"])
+input_string.split()
+
+print(len(input_string.split()), [f"{input_string}"])
+print(len(input_string), [f"{input_string}"])
