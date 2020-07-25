52e4a41822b41d860a4e12c5db9b03c1dbd27adb
gianninapg@gmail.com <Gi@090488>
2020-07-25 11:56:06 +1000

    create names_list

---
+++ names_list.py
@@ -0,0 +1,29 @@
+# #name = input("What is your name?")
+
+# #print(name)
+
+# #name = [input(f" Hello {name}")]
+
+# names_list = [
+#     "Izzy", "Archie", "Boston"
+# ]
+
+# name = input(names_list)
+
+# print(names_list)
+
+# #print(names_list)
+
+# print(name.append(name))
+# #print(name)
+
+
+input_names = input("Enter three names")
+names_list = input_names
+
+#print("\n")
+print("Printing names list")
+print(input_names)
+#print("Printing names list")
+#for name in names_list:
+    #print(name)
